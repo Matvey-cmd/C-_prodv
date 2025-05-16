818ab6539d5305c79f7f972fd2269f3f3e6c0ef3
Matvey-cmd <bolshakov.ma@phystech.edu>
2025-05-16 09:45:19 +0300

    Cmake

---
+++ cmake-build-debug/CMakeFiles/lib_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib_example.dir/homework/Task1.cpp.o"
+  "CMakeFiles/lib_example.dir/homework/Task1.cpp.o.d"
+  "lib_example"
+  "lib_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
